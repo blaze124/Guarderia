439a3a67879f85d6bb6be0cad4adc017b357d2f1
nicolasin <nicomartos@msn.com>
2015-01-06 00:06:21 +0100

    Eliminar Contenido2

--- application/views/verContenido.php
+++ application/views/verContenido.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+<link href="http://localhost/Guarderia/css/estilosPrincipal.css" type="text/css" rel="stylesheet">
 <meta charset="utf-8">
 <title>Documento sin título</title>
 </head>
@@ -11,12 +12,12 @@
 	echo '<h3>Nuevos Contenidos<h3>';
 	foreach($res as $valor)
 	{
-		echo '<div class="Contenido">';
+		echo '<div class="contenido">';
 		echo '<center><div class="titulo"><h2>'.$valor['titular'].'</h2></div>';
-		echo '<div class="img_cont">'.$valor['ruta'].'</div>'.'<br>';
+		echo '<div class="img_cont">'.$valor['ruta'].'</div>';
 		echo '<img src="'.$valor['ruta'].'"/>';
 		echo '<div class="text_cont"><p>'.$valor['cuerpo'].'</p></div>';
-		echo '<div class="text_cont"><p>'.$valor['tipo'].'</p></div>';
+		echo '<div class="tipo">'.$valor['tipo'].'</div>';
 		echo '<div class="fecha">'.$valor['fecha'].'</div>';
 		echo '</center></div>';
 	}
